5018780918be13acf4081e3fbb8ccc90f57428f7
Joe Sauve <joe.sauve@xamarin.com>
2016-01-15 11:21:34 -0600

    Updated ObservableObject to use hot new C# features and be more syntactically succinct. Updated BaseViewModel to use new OnPropertyChanged() implementation.

--- MvvmHelpers/BaseViewModel.cs
+++ MvvmHelpers/BaseViewModel.cs
@@ -5,80 +5,104 @@
     /// </summary>
     public class BaseViewModel : ObservableObject
     {
-        string title = string.Empty;
-
+        string title;
         /// <summary>
         /// Gets or sets the title.
         /// </summary>
         /// <value>The title.</value>
-        public string Title
-        {
-            get { return title; }
-            set { SetProperty(ref title, value); }
+        public string Title {
+            get { return title; } 
+            set 
+            { 
+                if (title == value)
+                    return;
+                title = value;
+                OnPropertyChanged();
+            }
         }
 
-        string subTitle = string.Empty;
-
+        string subtitle;
         /// <summary>
         /// Gets or sets the subtitle.
         /// </summary>
         /// <value>The subtitle.</value>
-        public string Subtitle
-        {
-            get { return subTitle; }
-            set { SetProperty(ref subTitle, value); }
+        public string Subtitle {
+            get { return subtitle; } 
+            set 
+            { 
+                if (subtitle == value)
+                    return;
+                subtitle = value;
+                OnPropertyChanged();
+            }
         }
 
-        string icon = string.Empty;
-
+        string icon;
         /// <summary>
         /// Gets or sets the icon.
         /// </summary>
         /// <value>The icon.</value>
-        public string Icon
-        {
-            get { return icon; }
-            set { SetProperty(ref icon, value); }
+        public string Icon {
+            get { return icon; } 
+            set 
+            { 
+                if (icon == value)
+                    return;
+                icon = value;
+                OnPropertyChanged();
+            }
         }
 
         bool isBusy;
-
         /// <summary>
         /// Gets or sets a value indicating whether this instance is busy.
         /// </summary>
         /// <value><c>true</c> if this instance is busy; otherwise, <c>false</c>.</value>
         public bool IsBusy
         {
-            get { return isBusy; }
-            set 
+            get { return isBusy; } 
+            set
             { 
-                if (SetProperty(ref isBusy, value))
-                    IsNotBusy = !isBusy;
+                if (isBusy == value)
+                    return;
+                isBusy = value;
+                IsNotBusy = !isBusy;
+                OnPropertyChanged();
             }
         }
 
-        bool isNotBusy = true;
-
+        bool isNotBusy;
         /// <summary>
         /// Gets or sets a value indicating whether this instance is not busy.
         /// </summary>
         /// <value><c>true</c> if this instance is not busy; otherwise, <c>false</c>.</value>
-        public bool IsNotBusy
-        {
-            get { return isNotBusy; }
-            private set { SetProperty(ref isNotBusy, value); }
+        public bool IsNotBusy {
+            get { return isNotBusy; } 
+            set 
+            { 
+                if (isNotBusy == value)
+                    return;
+                isNotBusy = value;
+                isBusy = !isNotBusy;
+                OnPropertyChanged();
+            }
         }
 
-        bool canLoadMore = true;
-
-       /// <summary>
-       /// Gets or sets a value indicating whether this instance can load more.
-       /// </summary>
-       /// <value><c>true</c> if this instance can load more; otherwise, <c>false</c>.</value>
+        bool canLoadMore;
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance can load more.
+        /// </summary>
+        /// <value><c>true</c> if this instance can load more; otherwise, <c>false</c>.</value>
         public bool CanLoadMore
         {
-            get { return canLoadMore; }
-            set { SetProperty(ref canLoadMore, value); }
+            get { return canLoadMore; } 
+            set
+            { 
+                if (canLoadMore == value)
+                    return;
+                canLoadMore = value;
+                OnPropertyChanged();
+            }
         }
     }
 }
--- MvvmHelpers/ObservableObject.cs
+++ MvvmHelpers/ObservableObject.cs
@@ -11,38 +11,18 @@ namespace MvvmHelpers
     public class ObservableObject : INotifyPropertyChanged
     {
         /// <summary>
-        /// Sets the property.
-        /// </summary>
-        /// <returns><c>true</c>, if property was set, <c>false</c> otherwise.</returns>
-        /// <param name="backingStore">Backing store.</param>
-        /// <param name="value">Value.</param>
-        /// <param name="propertyName">Property name.</param>
-        /// <param name="onChanged">On changed.</param>
-        /// <typeparam name="T">The 1st type parameter.</typeparam>
-        protected bool SetProperty<T>(
-            ref T backingStore, T value,
-            [CallerMemberName]string propertyName = "",
-            Action onChanged = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(backingStore, value))
-                return false;
-
-            backingStore = value;
-            onChanged?.Invoke();
-            OnPropertyChanged(propertyName);
-            return true;
-        }
-
-        /// <summary>
         /// Occurs when property changed.
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
+
         /// <summary>
         /// Raises the property changed event.
         /// </summary>
-        /// <param name="propertyName">Property name.</param>
-        protected void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
-         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        /// <param name="property">Property.</param>
+        protected void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
 
     }
 }

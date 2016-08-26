8ca0f6fdde025da97a809ac848f0afb94b08b06b
AlexKven <kvenclassical@gmail.com>
2016-08-25 17:48:33 -0700

    Changed RemoveRange to (optionally) use remove mode instead of reset, with the caveat documented that the starting index will not be specified because the removed items are not guaranteed to be adjacent. RemoveRange still defaults to reset, so this doesn't break existing code.

--- MvvmHelpers.Tests/ObservableRangeTests.cs
+++ MvvmHelpers.Tests/ObservableRangeTests.cs
@@ -33,9 +33,34 @@ namespace MvvmHelpers.Tests
                                   orderby person.LastName
                                   group person by person.SortName into personGroup
                                   select new Grouping<string, Person>(personGroup.Key, personGroup);
-
+       
             grouped.AddRange(sorted);
            
+        }
+
+        [Test()]
+        public void RemoveRange_RemoveTest()
+        {
+            ObservableRangeCollection<int> collection = new ObservableRangeCollection<int>();
+            int[] toAdd = new[] { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3 };
+            int[] toRemove = new[] { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 0, 0 };
+            collection.AddRange(toAdd);
+            collection.CollectionChanged += (s, e) =>
+            {
+                if (e.Action != System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+                    Assert.Fail("RemoveRange didn't use Remove like requested.");
+                if (e.OldItems == null)
+                    Assert.Fail("OldItems should not be null.");
+                int[] expected = new int[] { 1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 7, 8, 9, 9 };
+                if (expected.Length != e.OldItems.Count)
+                    Assert.Fail("Expected and actual OldItems don't match.");
+                for (int i = 0; i < expected.Length; i++)
+                {
+                    if (expected[i] != (int)e.OldItems[i])
+                        Assert.Fail("Expected and actual OldItems don't match.");
+                }
+            };
+            collection.RemoveRange(toRemove, System.Collections.Specialized.NotifyCollectionChangedAction.Remove);
         }
     }
 }

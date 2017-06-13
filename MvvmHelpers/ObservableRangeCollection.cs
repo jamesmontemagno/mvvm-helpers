9bfc0b0dbff64520a8eb4d0f83ea428c18e4fccd
Bryan Sanchez <Zidame2@hotmail.com>
2017-06-13 14:56:00 -0500

    Missing namespace

--- MvvmHelpers/ObservableRangeCollection.cs
+++ MvvmHelpers/ObservableRangeCollection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Linq;
 
 
 namespace MvvmHelpers

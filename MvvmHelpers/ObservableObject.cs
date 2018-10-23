d95790ee6a581339a6f3153a3c172a54d16ff247
Carlos Pueyo <cpueyo@nerviaconsultores.com>
2018-10-23 10:29:05 +0200

    Added SetProperty to update a property of class model

--- MvvmHelpers/ObservableObject.cs
+++ MvvmHelpers/ObservableObject.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MvvmHelpers
 {
@@ -40,12 +41,45 @@ namespace MvvmHelpers
             return true;
         }
 
+        /// <summary>
+        /// Sets a property in class
+        /// </summary>
+        /// <typeparam name="T">Type of model class</typeparam>
+        /// <typeparam name="X">Type of property to update</typeparam>
+        /// <param name="backingStore">Model object</param>
+        /// <param name="value">Value of property</param>
+        /// <param name="propertyName">Name of property to update</param>
+        /// <param name="onChanged">Event to launch</param>
+        /// <returns><c>true</c>, if property was set, <c>false</c> otherwise.</returns>
+        protected virtual bool SetProperty<T, X>(
+            ref T backingStore,
+            X value,
+            string propertyName,
+            Action onChanged = null) where T : class
+        {
+            var property = typeof(T).GetProperties().FirstOrDefault(f => f.Name.ToLowerInvariant() == propertyName.ToLowerInvariant());
+            if (property == null) return false;
 
-      /// <summary>
-      /// Occurs when property changed.
-      /// </summary>
-      public event PropertyChangedEventHandler PropertyChanged;
-        
+            var originalValue = property.GetValue(backingStore)?.ToString() ?? "";
+            var newValue = value?.ToString() ?? "";
+
+            // if value didn't change
+            if (originalValue == newValue) return false;
+
+            // Assign values
+            property.SetValue(backingStore, value);
+
+            // Event launching
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        /// <summary>
+        /// Occurs when property changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
         /// <summary>
         /// Raises the property changed event.
         /// </summary>

9af45f26b08697db0942975a8868e0458f0194e3
Adam Diament <adam@clevertree.io>
2020-06-16 12:29:09 +0300

    Add RaiseCanExecuteChanged to IAsyncCommand definition
    Re https://github.com/jamesmontemagno/mvvm-helpers/issues/67#issuecomment-644232864

---
+++ MvvmHelpers.UnitTests/IAsyncCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvvmHelpers.Commands;
+using MvvmHelpers.Interfaces;
+
+namespace MvvmHelpers.UnitTests
+{
+	[TestClass]
+	public class IAsyncCommandTests
+	{
+		[TestMethod]
+		public void IAsyncCommand_CanRaiseCanExecuteChanged()
+		{
+			IAsyncCommand command = new AsyncCommand(null);
+			command.RaiseCanExecuteChanged();
+		}
+
+		[TestMethod]
+		public void IAsyncCommandT_CanRaiseCanExecuteChanged()
+		{
+			IAsyncCommand<string> command = new AsyncCommand<string>(null);
+			command.RaiseCanExecuteChanged();
+		}
+	}
+}

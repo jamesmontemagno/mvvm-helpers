132d0fcb1e6e2c96c8605f44eb8e792f994d3bd8
Adam Diament <adam@clevertree.io>
2020-06-16 12:37:19 +0300

    Addressing Test Failures

--- MvvmHelpers.UnitTests/IAsyncCommandTests.cs
+++ MvvmHelpers.UnitTests/IAsyncCommandTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MvvmHelpers.Commands;
 using MvvmHelpers.Interfaces;
 
@@ -10,14 +11,14 @@ namespace MvvmHelpers.UnitTests
 		[TestMethod]
 		public void IAsyncCommand_CanRaiseCanExecuteChanged()
 		{
-			IAsyncCommand command = new AsyncCommand(null);
+			IAsyncCommand command = new AsyncCommand(() => Task.CompletedTask);
 			command.RaiseCanExecuteChanged();
 		}
 
 		[TestMethod]
 		public void IAsyncCommandT_CanRaiseCanExecuteChanged()
 		{
-			IAsyncCommand<string> command = new AsyncCommand<string>(null);
+			IAsyncCommand<string> command = new AsyncCommand<string>(sender => Task.CompletedTask);
 			command.RaiseCanExecuteChanged();
 		}
 	}
